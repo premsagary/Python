1510ffb39c8e9c096f18abe07e157ed5388fb75a
prem <premsagaryelgoi@gmail.com>
2019-04-21 15:15:17 -0400

    Constructor and destructor

---
+++ Abstraction.py
@@ -0,0 +1,6 @@
+"""
+Abstraction is simplifying the complex reality by modelling classes appropriate to the problem
+Class abstraction means to separate class implementation from the use of class
+
+"""
+
