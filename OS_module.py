dd54e4a55e68f5f7c983aa1f074404bf7824db8d
prem <premsagaryelgoi@gmail.com>
2019-04-21 20:30:38 -0400

    os module

---
+++ OS_module.py
@@ -0,0 +1,20 @@
+import os
+
+os.getcwd()
+
+# os.chdir('directory') # change directory
+
+# os.mkdir('directory name')
+
+# os.rmdir('directory name')
+
+print(os.name)
+print(os.environ)
+print(os.getlogin())
+print(os.getppid()) # Parent Process ID
+
+# Joining paths
+
+os.path.join('/abc/xyz', '/bb/cc', 'tt/rr')
+
+
